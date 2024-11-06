65873754ad3025b6da98646e0cb8e8a60f0ca3bc
Antonio Figueroa <antonio_figueroa@dell.com>
2024-11-06 14:55:30 -0600

    - completes challenge set 6

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,27 +8,113 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+
+            if (word == null || words == null || words.Count() == 0) { 
+                return false;
+            }
+
+            bool exists = false;
+            foreach (string currentWord in words)
+            {
+                {
+                    if (currentWord == null) { continue; }
+
+                    if (ignoreCase == true)
+                    {
+                        if (currentWord.ToLower() == word.ToLower()) { return true; }
+                    }
+                    else
+                    {
+                        if (currentWord == word) { return true; }
+                    }
+                }
+            }
+            return exists;
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if (num <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            if (str == null || str == string.Empty) {
+                return -1;
+            }
+
+            IEnumerable<char> distincts = str.Distinct();
+
+            if (distincts.Count() == 0)
+            {
+                return -1;
+            }
+            char lastDistinct = new char();
+
+            foreach (char c in distincts) {
+                int count = str.Count(x => x == c);
+                if (count == 1) {
+                    lastDistinct = c;
+                }
+            }
+            return str.IndexOf(lastDistinct);
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            int maxConsecutiveCount = 0;
+            int currentCount = 0;
+            int previousNum = int.MinValue;
+
+            foreach (int num in numbers)
+            {
+                if (num == previousNum)
+                {
+                    currentCount++;
+                }
+                else
+                {
+                    currentCount = 1;
+                    previousNum = num;
+                }
+
+                if (currentCount > maxConsecutiveCount)
+                {
+                    maxConsecutiveCount = currentCount;
+                }
+            }
+
+            return maxConsecutiveCount;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            var result = new List<double>();
+            if (elements == null || elements.Count < n || n < 1)
+            {
+                return result.ToArray();
+            }
+
+            for (int i = n - 1; i < elements.Count;)
+            {
+                result.Add(elements[i]);
+                i += n;
+            }
+
+            return result.ToArray();
         }
     }
 }
